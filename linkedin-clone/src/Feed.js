11eeba4c53d7af6c59847d7ced26235af491870b
Sophie <sophiehickey123@gmail.com>
2021-01-09 14:45:15 +0000

    added recent txt to sidebar & feed input section

---
+++ linkedin-clone/src/Feed.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './Feed.css';
+import CreateIcon from "@material-ui/icons/Create";
+
+function Feed() {
+    return (
+        <div className="feed">
+            <div className="feed_inputContainer">
+                <div className="feed_input">
+                    <CreateIcon />
+                    <form>
+                        <input type="text" />
+                        <button type="submit">Send</button>                        
+                    </form>
+                </div>
+                <div className="feed_inputOptions">
+                    <InputOption/>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Feed
