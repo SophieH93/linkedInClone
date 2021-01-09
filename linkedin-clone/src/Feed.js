295d7ab306a0df90572eef3fe889afba2e95ecf7
Sophie <sophiehickey123@gmail.com>
2021-01-09 15:20:28 +0000

    added input opetions functionality

--- linkedin-clone/src/Feed.js
+++ linkedin-clone/src/Feed.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import './Feed.css';
 import CreateIcon from "@material-ui/icons/Create";
+import ImageIcon from "@material-ui/icons/Image";
+import InputOption from "./InputOption";
+import SubscriptionIcon from "@material-ui/icons/Subscriptions";
+import EventNoteIcon from "@material-ui/icons/EventNote";
+import CalendarViewDayIcon from "@material-ui/icons/CalendarViewDay";
+
+
 
 function Feed() {
     return (
@@ -14,7 +21,10 @@ function Feed() {
                     </form>
                 </div>
                 <div className="feed_inputOptions">
-                    <InputOption/>
+                    <InputOption Icon={ImageIcon} title="Photo" color="#70B5F9"/>
+                    <InputOption Icon={SubscriptionIcon} title="Video" color="#E7A33E"/>
+                    <InputOption Icon={EventNoteIcon} title="Event" color="#C0CBCD"/>
+                    <InputOption Icon={CalendarViewDayIcon} title="Write article" color="#7FC15E"/>
                 </div>
             </div>
         </div>
